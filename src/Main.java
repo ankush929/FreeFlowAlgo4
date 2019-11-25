e7a14b5226add5c829c867998b298bae145c2cd2
Ankush Wadhwa <ankushwadhwa@Ankushs-MacBook-Pro.local>
2019-11-25 15:12:35 -0500

    Support for input txt file and dynamic init of maze

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,31 @@
+import java.io.*;
+
 public class Main {
 
     static private int dir[][] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}; // directions : right, left, up, down respectively
     static int num , flag;
 
-    static int N = 5;
+    static int N = 7;
 
-    static char[][] s = {{'R', 'G', 'B', 'W'}, {'W', 'W', 'W', 'W'}, {'R', 'G', 'B', 'W'}, {'Y', 'W', 'W', 'Y'}};
+    static char[][] input = new char[N][N];
 
     static int vis[][] = new int[N][N];
 
     public static void main(String[] args) {
 
 
+        input = readFile("input1.txt");
         int x = 0;
         int y = 0;
         int f = 0;
 
         char ch = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++)
-                if (s[i][j] != 'W') {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[0].length; j++)
+                if (input[i][j] != 'W') {
                     x = i;
                     y = j;
-                    ch = s[i][j];
+                    ch = input[i][j];
                     f = 1;
                     break;
                 }
@@ -40,15 +43,47 @@ public class Main {
             System.out.println("not solvable\n");
     }
 
+    private static char[][] readFile(String fileName) {
+
+        char input[][] = new char [N][N];
+        File file = new File(fileName);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            int i =0;
+            while((line = br.readLine())!=null)
+            {
+                char[] lineChar = line.toCharArray();
+                for (int j = 0; j < N; j++) {
+                    input[i][j] = lineChar[j];
+                }
+                i++;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[0].length; j++) {
+                System.out.print(input[i][j]);
+            }
+            System.out.println();
+        }
+
+        return input;
+    }
+
     static int judge(int x, int y) {
-        if (0 <= x && x < 4 && 0 <= y && y < 4)
+        if (0 <= x && x < N && 0 <= y && y < N)
             return 0;
         return 1;
     }
 
     static void DFS(int x, int y, char ch) {
 
-        if (s[x][y] != 'W' && s[x][y] != ch)
+        if (input[x][y] != 'W' && input[x][y] != ch)
             return ;
         if (judge(x, y) != 0 || vis[x][y] != 0) {
             System.out.println("Reached 2");
@@ -58,36 +93,36 @@ public class Main {
         num++;
         vis[x][y] = 1;
 
-        if (num >= 16) {
+        if (num >= N*N) {
             flag = 1;
             return;
         }
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < dir.length; i++) {
 
             int ax = x + dir[i][0];
             int ay = y + dir[i][1];
             if (judge(ax, ay) == 0 && vis[ax][ay] == 0) {
-                if (s[ax][ay] == ch)
+                if (input[ax][ay] == ch)
                 {
                     num++;
                     vis[ax][ay] = 1;
-                    if (num == 16) {
+                    if (num == N*N) {
                         System.out.println("Reached 2");
                         flag = 1;
                         return;
                     }
-                    for (int ii = 0; ii < 4; ii++)
-                        for (int jj = 0; jj < 4; jj++)
-                            if (s[ii][jj] != 'W' && vis[ii][jj] == 0 && s[ii][jj] != ch) {
+                    for (int ii = 0; ii < N; ii++)
+                        for (int jj = 0; jj < N; jj++)
+                            if (input[ii][jj] != 'W' && vis[ii][jj] == 0 && input[ii][jj] != ch) {
                                 int a = ii;
                                 int b = jj;
-                                char c = s[ii][jj];
+                                char c = input[ii][jj];
                                 DFS(a, b, c);
                             }
                     num--;
                     vis[ax][ay] = 0;
                 } else {
-                    if (s[ax][ay] != 'W')
+                    if (input[ax][ay] != 'W')
                        continue;
                     else
                         DFS(ax,ay,ch);
